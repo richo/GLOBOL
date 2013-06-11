3d3464116b2ac8b995a2cf6e4a3d8916e1cf42a8
Richard Bone <rjonbone@gmail.com>
2013-06-11 17:04:07 +1000

    Fix formatting

--- src/globol/parser/tokens.go
+++ src/globol/parser/tokens.go
@@ -1,17 +1,17 @@
 package parser
 
 const (
-    NODE_COMMENT = iota
+ 	 NODE_COMMENT = iota
 )
 
 var COMMENT_MARKERS = [...]string{"DID I EVER TELL YOU",
-    "IGNORE THIS NEXT PART",
-    "SO ANYWAY,",
-    "YOU KNOW,",
-    "BY THE WAY,",
-    "OF COURSE,",
-    "INTERESTINGLY ENOUGH,",
-    "ERGO"}
+ 	 "IGNORE THIS NEXT PART",
+ 	 "SO ANYWAY,",
+ 	 "YOU KNOW,",
+ 	 "BY THE WAY,",
+ 	 "OF COURSE,",
+ 	 "INTERESTINGLY ENOUGH,",
+ 	 "ERGO"}
 
 
 /* type NODE_COMMENT */
